c46d45cec3a28b42893d0fffc53f03f6cd7292a6
dvu <dinh.vu@agoda.com>
2021-10-11 12:12:48 +0700

    Add code to load native library dependings on OS annd arch

--- src/main/java/www/jasmine/Main.java
+++ src/main/java/www/jasmine/Main.java
@@ -7,10 +7,29 @@ import java.io.IOException;
 import java.util.logging.Logger;
 
 public class Main {
-    // ToDo: load suitable version of libpcap depends on OS and architecture 32/64 bits
     static {
+        String nameOfOS = System.getProperty("os.name").toLowerCase();
+        String bitnessOfJVM = System.getProperty("os.arch");
+
         try {
-            System.load("/tmp/libpcap64.so");
+            // Tested on Centos OS, OpenJDK 64-Bit
+            if ((nameOfOS.contains("nix") || nameOfOS.contains("nux") || nameOfOS.contains("aix"))) {
+                if (bitnessOfJVM.contains("64")) {
+                    System.load("/tmp/libpcap64.so");
+                } else {
+                    System.load("/tmp/libpcap64.so");
+                }
+            }
+            // ToDo: Test in Windows machine, which I do not have at the moment
+            //       Note that there are 2 different versions of Packet.dll and wpcap.dll: 64 and 32-bit
+            if (nameOfOS.contains("win")) {
+                System.load("Packet.dll");
+                System.load("wpcap.dll");
+            }
+            // Tested on Mac OSX BigSur, OpenJDK 64-Bit
+            if (nameOfOS.contains("mac")) {
+                System.load("/tmp/libpcap.1.8.1.dylib");
+            }
         } catch (UnsatisfiedLinkError e) {
             System.err.println("Native code library failed to load.\n" + e);
             System.exit(1);
